920ee3990be5917881528c0d8d9521d47f32b91f
ediaz13 <diazemanuel27@gmail.com>
2021-08-16 20:07:30 -0300

    Shortest Path Using Topological Ordering!

---
+++ src/TopologicalOrderingShortestPath/Edge.java
@@ -0,0 +1,20 @@
+package TopologicalOrderingShortestPath;
+
+public class Edge {
+    private Vertex target;
+    private int weight;
+
+    public Edge(Vertex target, int wight) {
+        this.target = target;
+        this.weight = wight;
+    }
+
+    public Vertex getTarget() {
+        return target;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+}
